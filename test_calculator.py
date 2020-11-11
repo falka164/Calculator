ea4cd225267b9f7728a2e72433574fbc81509aac
falka164 <falka164@gmail.com>
2020-11-11 18:48:21 +0100

    add tests, requirements file

---
+++ test_calculator.py
@@ -0,0 +1,10 @@
+import calculator
+
+
+class TestCalculator:
+
+    def test_add(self):
+        assert 4 == calculator.add(2, 2)
+
+    def test_substract(self):
+        assert 2 == calculator.substract(4, 2)
