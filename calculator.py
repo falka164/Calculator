ed79eb4e9dc7288adb173c8a7035f0550aad9dae
falka164 <falka164@gmail.com>
2020-11-11 23:35:28 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,10 @@
 def add(num1, num2):
-    return num1 + num2
+    mySum = num1 + num2
+    print("I've just added two numbers! My result is: " + mySum)
+    return mySum
 
 
 def substract(num1, num2):
-    return num1 - num2
+    mySub = num1 - num2
+    print("I've just substracted two numbers! My result is: " + mySub)
+    return mySub
